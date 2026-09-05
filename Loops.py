7bb0b7b6bab14dd3e6337c322feabee93c94aecb
Cakeguy14 <senthurnethra123@gmail.com>
2026-09-05 21:09:27 +0530

    loops new

--- Loops.py
+++ Loops.py
@@ -39,3 +39,71 @@ for i in range(4):
 
 print("Hi\n" * 4, end="") - #prints "Hi" 4 times, each on a new line
 
+TODO:
+#while loop - repeats a block of code as long as a condition is true
+
+while True:
+    n = int(input("Enter a positive integer: "))
+    if n > 0:
+        break
+
+for _ in range(n):
+    print("Hi")
+
+#using functions to avoid code repetition
+
+def main():
+    meow(3)
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+main() - #calls the main function
+
+#or
+
+def main():
+    number = get_number()
+    meow(number)
+
+def get_number():
+    while True:
+        n = int(input("Enter a positive integer: "))
+        if n > 0:
+            return n
+
+def meow(n):
+    for _ in range(n):
+        print("Meow")
+
+main()
+
+TODO:
+#list - a collection of items
+
+students = ["Hermoine", "Harry", "Ron"]
+
+for student in students:
+    print(student)
+
+#len() - returns the number of items in a list
+
+print(len(students)) - #prints 3
+
+for student in range(len(students)): #range() - returns a sequence of numbers, starting from 0 by default, and increments by 1 (by default), and stops before a specified number
+    print(student + 1, students[student]) - #prints each student in the list
+
+#dict - a collection of key-value pairs
+
+students = {
+    "Hermoine": "Granger",
+    "Harry": "Potter",
+    "Ron": "Weasley"
+}
+
+print(students["Hermoine"])  #unlike lists, dictionaries are unordered, so you cannot access items by index, but by key
+
+for each in students:
+    print(each, students[each])  #prints each key-value pair in the dictionary
+    print(each, students[each], sep=", ")
